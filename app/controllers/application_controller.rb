3123cf8354f004b30c73ae96a4ece37d2d622120
dorabby <dorabbi1010@gmail.com>
2020-02-13 21:14:14 +0900

    topview

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:family_name, :name, :family_name_furigana, :name_furigana])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:family_name, :name, :family_name_furigana, :name_furigana])
+  # end
 end
