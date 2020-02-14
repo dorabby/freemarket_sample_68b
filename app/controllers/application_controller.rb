1641fad0499f47ca3ec00b1537317faf3a9b7db4
anh-210 <59760647+anh-210@users.noreply.github.com>
2020-02-14 15:07:09 +0900

    create view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:family_name, :name, :family_name_furigana, :name_furigana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :name, :family_name_furigana, :name_furigana, :birthday])
   end
 end
+
