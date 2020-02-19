68f624688e38f26d16d7074e6d8a68ed6ce836d5
syk227 <syk.m1998@gmail.com>
2020-02-19 20:49:39 +0900

    mypage

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
       post 'delete', to: 'card#delete'
     end
   end
-  resources :users, only: [:destroy]
+  resources :users, only: [:show,:edit,:update,:destroy]
   resources :items, only: [:new,:create,:show]
-
 end
