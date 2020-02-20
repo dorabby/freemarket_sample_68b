53e1a3757705ce19510a2eb7cebd930cb14a112c
dorabby <dorabbi1010@gmail.com>
2020-02-20 14:26:50 +0900

    カテゴリーに関連するrouteの追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show,:edit,:update,:destroy]
-  resources :items, only: [:new,:create,:show]
-
+  resources :items do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren',defaults: { format: 'json' }
+    end
+  end
 end
