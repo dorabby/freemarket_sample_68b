6852b1f5f71a869ad82c65d7e23a49d3553285bd
dorabby <dorabbi1010@gmail.com>
2020-02-20 14:11:30 +0900

    商品出品画像投稿UI

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,3 +1,7 @@
+
+
+
+
 $(document).on('turbolinks:load', function() {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
@@ -16,7 +20,7 @@ $(document).on('turbolinks:load', function() {
   }
 
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4];
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
@@ -58,86 +62,4 @@ $(document).on('turbolinks:load', function() {
 });
 
 
-//以下カテゴリ
-// $(document).on('turbolinks:load', function() {
-//   $(function(){
-//     function appendOption(category){
-//       var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
-//       return html;
-//     }
-//     // 子カテゴリーを表示するビュー
-//     function appendChidrenBox(insertHTML){
-//       var childSelectHtml = '';
-//       childSelectHtml = `<select name='item[child_category_id]' class="select-default" id="child-form" required>
-//                               <option label="---" data-category="---"></option>
-//                               ${insertHTML}
-//                             </select>`
-//       $('#category').append(childSelectHtml);
-//     }
-//     // 孫カテゴリーを表示するビュー
-//     function appendGrandchidrenBox(insertHTML){
-//       var grandchildSelectHtml = '';
-//       grandchildSelectHtml = `<select name='item[category_id]' class="select-default" id="grandchild-form" required>
-//                                   <option label="---" data-category="---"></option>
-//                                   ${insertHTML}
-//                                 </select>`;
-//       $('#category').append(grandchildSelectHtml);
-//     }
-//     // 親カテゴリー選択のイベント
-//     $('#parent-form').on('change', function(){
-//       var parentCategory = $('option:selected',this).val();
-//       if (parentCategory != "---"){
-//         $.ajax({
-//           url: '/items/get_category_children',
-//           type: 'GET',
-//           data: { parent_id: parentCategory },
-//           dataType: 'json'
-//         })
-//         .done(function(children){
-//           $('#child-form').remove();
-//           $('#grandchild-form').remove();
-//           var insertHTML = '';
-//           children.forEach(function(child){
-            
-//             insertHTML += appendOption(child);
-//           });
-//           appendChidrenBox(insertHTML);
-//         })
-//         .fail(function(){
-//           alert('カテゴリー取得に失敗しました');
-//         })
-//       }else{
-//         $('#child-form').remove();
-//         $('#grandchild-form').remove();
-//       }
-//     });
-//     // 子カテゴリー選択後のイベント
-//     $('#category').on('change', "#child-form",function(){
-//       var childId = $('option:selected',this).val();
-//       if (childId != "---"){
-//         $.ajax({
-//           url: '/items/get_category_grandchildren',
-//           type: 'GET',
-//           data: { child_id: childId },
-//           dataType: 'json'
-//         })
-//         .done(function(grandchildren){
-//           if (grandchildren.length != 0) {
-//             $('#grandchild-form').remove();
-//             var insertHTML = '';
-//             grandchildren.forEach(function(grandchild){
-//               insertHTML += appendOption(grandchild);
-//             });
-            
-//             appendGrandchidrenBox(insertHTML);
-//           }
-//         })
-//         .fail(function(){
-//           alert('カテゴリー取得に失敗しました');
-//         })
-//       }else{
-//         $('#grandchild-form').remove();
-//       }
-//     });
-//   });
-// });
+//以降カテゴリー
