d62de59669ca6ed9f47cf9ffbfcb6eea2853166d
dorabby <dorabbi1010@gmail.com>
2020-02-25 14:45:15 +0900

    テストの記述の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     category_id               {200}
     price                     {"1500"}
     condition                 {"未使用に近い"}
-    derivery_charge           {2}
-    days                      {2}
+    derivery_charge           {"送料込み（出品者負担）"}
+    days                      {"1~2日で発送"}
     prefecture_id             {1}
   end
 end
