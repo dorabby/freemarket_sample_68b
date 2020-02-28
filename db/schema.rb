5ab16130423501760aae0536fb55ee697fe32219
yasu1224 <yasu1224x@gmail.com>
2020-02-28 18:54:51 +0900

    deproy

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_110058) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "item_id"
-    t.text "text"
+    t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
