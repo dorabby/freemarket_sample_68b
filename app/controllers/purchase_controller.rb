048934d7984dd3d3f5e55bcac0b4d18ec2ab8682
dorabby <dorabbi1010@gmail.com>
2020-03-02 14:49:31 +0900

    修正2

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,27 +1,22 @@
 class PurchaseController < ApplicationController
+  before_action :set_item,only:[:index,:pay]
+  before_action :set_card,only:[:index,:pay]
 
   require 'payjp'
 
   def index
-    @user = current_user
     @address = Address.find_by(user_id: current_user.id)
-    @item = Item.find_by(id: params[:item_id])
-    @images = Image.where(item_id: @item.id)
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      # redirect_to controller: "card", action: "pay"
+    @images = @item.images
+    if @card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
     
   end
 
   def pay
-    @item = Item.find_by(id: params[:item_id])
-    @card = Card.where(user_id: current_user.id).first
-  
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => @item.price,
@@ -38,6 +33,17 @@ class PurchaseController < ApplicationController
   def  done 
     @item_purchaser= Item.find_by(id: params[:item_id])
     @item_purchaser.update( buyer_id: current_user.id)
-   end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_card
+    @card = Card.find_by(user_id: current_user.id)
+  end
+
 
 end
