d57430d3dc807d25045c6adbb902cea1f5eafa76
dorabby <dorabbi1010@gmail.com>
2020-03-03 17:18:51 +0900

    @commentの記載がなくなっていたのを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,12 +16,12 @@ class ItemsController < ApplicationController
 
 
   def show
-    @items = Item.includes(:images)
-    @item = Item.find_by(id: params[:id])
-    @saler = User.find_by(id: @item.saler_id)
-    @brand = Brand.find_by(id: @item.brand_id)
-    @category = Category.find_by(id: @item.category_id)
-    @images = Image.where(item_id: @item.id)
+    @saler = @item.saler
+    @brand = @item.brand
+    @category = @item.category
+    @images =  @item.images
+    @comment = Comment.new
+    @comments = @item.comments.includes(:user)
   end
 
   def new
